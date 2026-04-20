7bded94072a1b7a959910947df8c99f7e35f8820
iWeslax83 <emirsakarya00@gmail.com>
2026-04-20 14:53:22 +0300

    feat: add RateLimitBucket model with TTL index

---
+++ models/RateLimitBucket.js
@@ -0,0 +1,15 @@
+// models/RateLimitBucket.js
+import mongoose from "mongoose";
+
+const RateLimitBucketSchema = new mongoose.Schema(
+  {
+    key: { type: String, required: true, unique: true, index: true },
+    count: { type: Number, default: 0 },
+    windowStart: { type: Date, required: true },
+    expiresAt: { type: Date, required: true, index: { expires: 0 } },
+  },
+  { timestamps: false }
+);
+
+export default mongoose.models.RateLimitBucket ||
+  mongoose.model("RateLimitBucket", RateLimitBucketSchema);
